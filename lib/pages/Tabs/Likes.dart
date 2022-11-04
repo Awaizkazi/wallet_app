f8e5da6fe7786a12fc05b90f12657846e7373f3a
Awaizkazi <Owaizkazi789@gmail.com>
2022-11-05 00:12:48 +0530

    progress

--- lib/pages/Tabs/Likes.dart
+++ lib/pages/Tabs/Likes.dart
@@ -35,13 +35,13 @@ class _LikesState extends State<Likes> {
   Widget LikesContainer() {
     return Card(
       child: Row(
-        // ndfnosfhsohgsrhlhthrao
-        // ritioneahfiu   ohafhia hi hhiurh
-        // fiskfksbfkjsdb hiuaeibakjb i8whdhiuwha ihiuiu wa
         children: [
-          Image.network(
-            'https://media.istockphoto.com/photos/gateway-of-india-mumbai-maharashtra-monument-landmark-famous-place-picture-id1307189136?b=1&k=20&m=1307189136&s=170667a&w=0&h=F-BSrbduokK7pz1AEOwOGuoDe2xd28wnSyZpll4Jzks=',
-            width: 80,
+          ClipRRect(
+            borderRadius: BorderRadius.circular(5),
+            child: Image.network(
+              'https://media.istockphoto.com/photos/gateway-of-india-mumbai-maharashtra-monument-landmark-famous-place-picture-id1307189136?b=1&k=20&m=1307189136&s=170667a&w=0&h=F-BSrbduokK7pz1AEOwOGuoDe2xd28wnSyZpll4Jzks=',
+              width: 80,
+            ),
           ),
           SizedBox(
             width: 10,
@@ -57,15 +57,13 @@ class _LikesState extends State<Likes> {
                       style: TextStyle(color: Colors.grey[700], fontSize: 17),
                     ),
                     TextSpan(
-                      text: '25 September 2022 7:33PM',
-                      style: TextStyle(color: Colors.black, fontSize: 13),
+                      text: '25 September 2022  7:33PM',
+                      style: TextStyle(color: Colors.grey[800], fontSize: 13),
                     ),
                   ],
                 ),
               ),
-              SizedBox(
-                height: 3,
-              ),
+              SizedBox(height: 2),
               RichText(
                 text: TextSpan(
                   children: [
@@ -80,6 +78,7 @@ class _LikesState extends State<Likes> {
                   ],
                 ),
               ),
+              SizedBox(height: 2),
               RichText(
                 text: TextSpan(
                   children: [
@@ -91,6 +90,14 @@ class _LikesState extends State<Likes> {
                 ),
               ),
             ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 60),
+            child: IconButton(
+              iconSize: 18,
+              onPressed: () {},
+              icon: Icon(Icons.play_arrow),
+            ),
           ),
         ],
       ),
