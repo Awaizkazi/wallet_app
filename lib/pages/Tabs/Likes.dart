72b3bb2b6d3ecaca0468bf61466c021ae85671f9
Awaizkazi <Owaizkazi789@gmail.com>
2022-11-04 23:58:59 +0530

    c

--- lib/pages/Tabs/Likes.dart
+++ lib/pages/Tabs/Likes.dart
@@ -35,6 +35,7 @@ class _LikesState extends State<Likes> {
   Widget LikesContainer() {
     return Card(
       child: Row(
+        // ndfnosfhsohgsrhlhthrao
         children: [
           Image.network(
             'https://media.istockphoto.com/photos/gateway-of-india-mumbai-maharashtra-monument-landmark-famous-place-picture-id1307189136?b=1&k=20&m=1307189136&s=170667a&w=0&h=F-BSrbduokK7pz1AEOwOGuoDe2xd28wnSyZpll4Jzks=',
